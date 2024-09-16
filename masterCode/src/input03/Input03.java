c8156d7b4ea9799a1df9a0e2e6bdd1879d8010ff
Kimi730 <1213284@student.roc-nijmegen.nl>
2024-09-16 14:43:45 +0200

    ds

---
+++ masterCode/src/input03/Input03.java
@@ -0,0 +1,19 @@
+package input03;
+
+import java.util.Scanner;
+
+class Input03 {
+
+    public static void main(String[] args) {
+      
+        Scanner sc = new Scanner(System.in);
+        
+       int x = sc.nextInt(5);
+       int y = sc.nextInt(4);
+       int z = sc.nextInt(7);
+        
+        System.out.println(x);
+        System.out.println();
+        sc.close();
+    }
+}
---
+++ masterCode/src/variables03/Variables03.java
@@ -0,0 +1,13 @@
+package variables03;
+
+public class Variables03 {
+
+    public static void main(String[] args) {
+        
+    	boolean Bool = true;
+int rodePin = 1;
+int geelPin = 2;
+int blauwPin = 3;
+
+    }
+}
