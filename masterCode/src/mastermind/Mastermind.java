57149d15f2e870db7a046338cfe7f660a425f060
Kimi730 <1213284@student.roc-nijmegen.nl>
2024-10-07 13:02:43 +0200

    .

---
+++ masterCode/src/mastermind/Mastermind.java
@@ -0,0 +1,25 @@
+package mastermind;
+
+public class Mastermind {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String red = ("Red");
+        String yellow = ("Geel");
+        String green = ("Groen");
+        String blue = ("Blauw");
+        String purple = ("Paars");
+        String white = ("Wit");
+        String black = ("Zwart");
+        
+        String code = ();
+        
+        System.out.println("Welcome to Mastermind, please put in four codes, you have 5 chances");
+        System.out.println("Succes !");
+        System.out.println(C);
+        
+       
+
+	}
+
+}
