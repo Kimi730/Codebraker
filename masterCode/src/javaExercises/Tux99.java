16957af48c76a04d0e53d6b296898e83978eda20
Kimi730 <1213284@student.roc-nijmegen.nl>
2024-11-08 13:48:56 +0100

    .

---
+++ masterCode/src/javaExercises/Tux99.java
@@ -0,0 +1,22 @@
+package javaExercises;
+
+public class Tux99 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//for (int woord= 0; woord <=99; woord++) {
+//	System.out.println(woord + " TUX!");
+//}
+		
+		
+		String daysWeek[]= {"Maandag","Dinsdag","Woensdag","Donderdag","Vrijdag","Zaterdag","Zondag"};
+		
+		for (int week =1; week <= 7; week++) {
+			System.out.println( daysWeek + " " + week);
+			
+		}
+			
+	}
+
+}
